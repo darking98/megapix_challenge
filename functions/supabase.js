3eb6bfc2f14169da5a4e8b4789de56916453c8af
Diego Rodríguez <diego.gabrielrodriguez@hotmail.com>
2024-01-18 08:09:29 -0300

    agregando validacion para enviar otp de mail segun variable de entorno

--- functions/supabase.js
+++ functions/supabase.js
@@ -1,4 +1,3 @@
-//import { createClient } from '@/utils/supabase/server'
 import { createClient } from '@supabase/supabase-js'
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
@@ -17,7 +16,11 @@ export async function signInWithEmail(email) {
     options: {
       // set this to false if you do not want the user to be automatically signed up
       shouldCreateUser: false,
-      emailRedirectTo: 'http://localhost:3000/dashboard'
+      emailRedirectTo:
+        process.env.NEXT_PUBLIC_ENV === 'local'
+          ? 'http://localhost:3000/dashboard'
+          : process.env.NEXT_PUBLIC_ENV === 'production' &&
+            'https://megapix-challenge.vercel.app/dashboard'
     }
   })
 
